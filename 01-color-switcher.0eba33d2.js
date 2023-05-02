549a24855359bf29feb180c8ff8398dbf154157b
Enab13d <120662026+Enab13d@users.noreply.github.com>
2023-05-02 19:53:51 +0000

    Deploying to gh-pages from @ Enab13d/goit-js-hw-09@7f3bff06007ed44ca4abac08f5bb6020324445eb 🚀

--- 01-color-switcher.0eba33d2.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let e=null,o=!1;t.startBtn.addEventListener("click",(()=>{o||(o=!0,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3))})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),o=!1}));
-//# sourceMappingURL=01-color-switcher.0eba33d2.js.map
